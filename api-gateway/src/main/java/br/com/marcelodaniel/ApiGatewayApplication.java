ffce48069a21150362d0c588caa935b545f42156
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2021-12-08 15:46:36 -0300

    updates

--- api-gateway/src/main/java/br/com/marcelodaniel/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/marcelodaniel/ApiGatewayApplication.java
@@ -2,9 +2,9 @@ package br.com.marcelodaniel;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut.run(ApiGatewayApplication.class, args);
     }
 }
